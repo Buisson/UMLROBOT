07583d3e87c19de2ce682411369b3884eae62c73
Yassine Tijani <tijani.yassine@etu.unice.fr>
2015-11-10 16:21:06 +0100

    fuck you

--- Etat.h
+++ Etat.h
@@ -1,13 +1,13 @@
 class Etat{
 	public:
 		class ImpossibleAction{};
-		Etat avancer();
-		Etat tourner();
-		Etat evaluerPlot();
-		Etat saisir();
-		Etat poser();
-		Etat peser();
-		Etat rencontrerPlot();
-		Etat figer();
-		Etat repartir();
+		Etat& avancer();
+		Etat& tourner();
+		Etat& evaluerPlot();
+		Etat& saisir();
+		Etat& poser();
+		Etat& peser();
+		Etat& rencontrerPlot();
+		Etat& figer();
+		Etat& repartir();
 };
