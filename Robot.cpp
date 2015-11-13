b855a4f41df4e654e5750a5e872a4d5f16853887
Buisson <colombet.aurelien@etu.unice.fr>
2015-11-13 14:57:22 +0100

    Debug moi ça!

--- Robot.cpp
+++ Robot.cpp
@@ -1,14 +1,5 @@
 #include "Robot.h"
 
-/*
-Robot::Robot(string d,Position po, Plot pl, Objet objet, Etat e){
-	dir=d;
-	pos=po;
-	plot=pl;
-	obj=objet;
-	etat=e;
-}*/
-
 
 void Robot::avancer(int x, int y){
 	try{
